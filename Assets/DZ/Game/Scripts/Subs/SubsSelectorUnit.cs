a172e4b41cb50c2d519425e42c541f72b43d0da6
Vladislav Pazych <vladpazych@gmail.com>
2018-01-28 05:17:31 +0200

    Add improved writer

--- Assets/DZ/Game/Scripts/Subs/SubsSelectorUnit.cs
+++ Assets/DZ/Game/Scripts/Subs/SubsSelectorUnit.cs
@@ -7,31 +7,36 @@ namespace DZ.Game.Scripts
 {
 	public class SubsSelectorUnit : MonoBehaviour
 	{
+		private Animator __animator;
+		protected Animator _animator { get { if (__animator == null) { __animator = GetComponent<Animator>(); } return __animator; } }
+
 		private RectTransform __rectTransform;
 		protected RectTransform _rectTransform { get { if (__rectTransform == null) { __rectTransform = GetComponent<RectTransform>(); } return __rectTransform; } }
 
-		public float downRotation;
-		public float upRotation;
+		// public float downRotation;
+		// public float upRotation;
 
-		[Header("Down")]
-		public float downDuration;
-		public Ease downEasing;
-		[Header("Up")]
-		public float upDuration;
-		public Ease upEasing;
+		// [Header("Down")]
+		// public float downDuration;
+		// public Ease downEasing;
+		// [Header("Up")]
+		// public float upDuration;
+		// public Ease upEasing;
 
 		public void SetSelectction(bool value)
 		{
-			_rectTransform.DOKill(false);
+			_animator.SetBool("Active", value);
 
-			if (value)
-			{
-				_rectTransform.DOLocalRotate(new Vector3(0, 0, downRotation), downDuration).SetEase(downEasing);
-			}
-			else
-			{
-				_rectTransform.DOLocalRotate(new Vector3(0, 0, upRotation), upDuration).SetEase(upEasing);
-			}
+			// _rectTransform.DOKill(false);
+
+			// if (value)
+			// {
+			// 	_rectTransform.DOLocalRotate(new Vector3(0, 0, downRotation), downDuration).SetEase(downEasing);
+			// }
+			// else
+			// {
+			// 	_rectTransform.DOLocalRotate(new Vector3(0, 0, upRotation), upDuration).SetEase(upEasing);
+			// }
 		}
 	}
 }
